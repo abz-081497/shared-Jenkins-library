716e44f080c53660710574d18924c7e00021d4a4
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-12 17:47:20 +0800

    Create uploadAllFileS3.groovy

---
+++ vars/uploadAllFileS3.groovy
@@ -0,0 +1,9 @@
+def call(Map stageParams = [:]) {
+    withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {       
+        s3Upload( 
+            includePathPattern: "**/*",
+            bucket: "${stageParams.s3Bucket}"            
+        )
+    }
+}
+//awsRegion: "us-east-1", bucket: "testbucket-darren"
