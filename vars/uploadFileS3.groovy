28d941fa23f3a4244b3f205bb9712820c4162eb3
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-12 17:46:31 +0800

    Delete uploadFileS3.groovy

--- vars/uploadFileS3.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-abigael"]) {
-    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {       
-        s3Upload( 
-            includePathPattern: "**/*",
-            bucket: stageParams.s3Bucket            
-        )
-    }
-}
