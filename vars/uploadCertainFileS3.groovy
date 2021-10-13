509f8e3cd6fb30789c9db1115a90ebfaeed32951
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-13 17:05:34 +0800

    Update uploadCertainFileS3.groovy

--- vars/uploadCertainFileS3.groovy
+++ vars/uploadCertainFileS3.groovy
@@ -1,8 +1,8 @@
 def call(Map stageParams = [:]) {
     withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {       
         s3Upload( 
-            bucket: "${stageParams.s3Bucket}",
-            file: "${stageParams.certainFile}"
+            bucket: stageParams.s3Bucket,
+            file: stageParams.certainFile                       
         )
     }
 }
