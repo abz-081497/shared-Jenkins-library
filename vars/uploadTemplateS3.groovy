5f632cb104ff69ee3df84e786080f69a9caa5da5
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-13 16:58:27 +0800

    Update uploadTemplateS3.groovy

--- vars/uploadTemplateS3.groovy
+++ vars/uploadTemplateS3.groovy
@@ -2,8 +2,11 @@ def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-abigae
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
-            includePathPattern: "*.yml",
-            bucket: stageParams.s3Bucket
+            includePathPattern: "${stageParams.pathFolder}",
+            bucket: "${stageParams.s3Bucket}"
+                      
         )
     }
 }
+//ito yong path nong para sa pag upload ng folder
+// pathFolder: "*.yml, src/HelloAbby.txt"
